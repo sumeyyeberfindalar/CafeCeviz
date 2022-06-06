b4e73f4ac48f8db729a1fb07a76a0fe0046a2214
sumeyyeberfindalar <berfindalar@gmail.com>
2022-06-06 22:46:51 +0300

    Cafe Otomasyonu
    2021-2022 Görsel Programlama dersi proje ödevi, otomasyon programı.

---
+++ CafeCeviz/CafeCeviz/frmMutfak.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace CafeCeviz
+{
+    partial class frmMutfak
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmMutfak
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 514);
+            this.Name = "frmMutfak";
+            this.Text = "frmMutfak";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
